a0bb397c0a0e50f9a133adda3de102aad86ad39f
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-02-05 17:46:34 +0200

    ADD Progress component

---
+++ src/components/Progress.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Progress({
+  index,
+  numOfQuestions,
+  points,
+  maxPossiblePoints,
+  answer,
+}) {
+  return (
+    <header className="progress">
+      <progress max={numOfQuestions} value={index + Number(answer != null)} />
+      <p>
+        Question <strong> {index} </strong> / {numOfQuestions}
+      </p>
+      <p>
+        {" "}
+        <strong> {points} </strong> / {maxPossiblePoints}
+      </p>
+    </header>
+  );
+}
