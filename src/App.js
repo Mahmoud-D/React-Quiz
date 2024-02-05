037a224c7df4250290bfac355c8da8e04d7e93e5
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-02-05 17:49:33 +0200

    ADD to App some changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import { useEffect, useReducer } from "react";
 import "./App.css";
-import Header from "./Header.js";
-import Main from "./Main";
+import Header from "./components/Header.js";
+import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen";
+import Question from "./components/Question";
+import Nextbutton from "./components/Nextbutton";
+import Progress from "./components/Progress";
+import FinishScreen from "./components/FinishScreen";
 
 const initialState = {
   questions: [],
 
   // 'loading', 'error', 'ready', 'active', 'complete'
   status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -23,27 +33,90 @@ function reducer(state, action) {
         ...state,
         status: "failed",
       };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+      };
+
     default:
       throw new Error(" Unknown action");
   }
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const numOfQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
   useEffect(function () {
-    fetch("http://localhost:8000/questions").then((res) =>
-      res
-        .json()
-        .then((data) => dispatch({ type: "dataReceived", payload: data }))
-        .catch((err) => dispatch({ type: "dataFailed" }))
-    );
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
   return (
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Qestion</p>
+        {status === "loading" && <Loader />}
+        {status === "failed" && <Error />}
+        {status === "ready" && (
+          <StartScreen numOfQuestions={numOfQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numOfQuestions={numOfQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Question
+              Question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <Nextbutton
+              dispatch={dispatch}
+              answer={answer}
+              index={index}
+              numOfQuestions={numOfQuestions}
+            />
+          </>
+        )}
+        <br></br>
+        {status === "finished" && (
+          <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} />
+        )}
       </Main>
     </div>
   );
